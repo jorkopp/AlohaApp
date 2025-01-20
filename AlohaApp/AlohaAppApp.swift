81b5016c71383cc56301c9894418b17e84d9c621
Jordan Kopp <jorkopp@gmail.com>
2025-01-19 17:50:08 -0800

    Initial Commit

---
+++ AlohaApp/AlohaAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlohaAppApp.swift
+//  AlohaApp
+//
+//  Created by Jordan Kopp on 1/19/25.
+//
+
+import SwiftUI
+
+@main
+struct AlohaAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AlohaAppTests/AlohaAppTests.swift
@@ -0,0 +1,16 @@
+//
+//  AlohaAppTests.swift
+//  AlohaAppTests
+//
+//  Created by Jordan Kopp on 1/19/25.
+//
+
+import Testing
+
+struct AlohaAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
